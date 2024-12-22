862c4f7a3068d83a45487329d881b3de11cfa61e
silvadias <luisdias04@hotmail.com>
2024-12-22 13:36:18 -0300

    bug: Restruturação do servidor e portas.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
+require('dotenv').config(); // Carregar variáveis de ambiente do .env
 const app = require('./app');
-const db = require('./config/db');
+const db = require('./config/db'); // Importa a configuração do banco de dados
 const { PORT } = require('./config/env');
 
+// Conectar ao banco de dados e iniciar o servidor
 db.authenticate()
   .then(() => console.log('✅ Conectado ao banco de dados MySQL!'))
   .catch(err => console.error('❌ Erro ao conectar ao MySQL:', err));
 
-db.sync()
+db.sync() // Sincroniza o banco (cria as tabelas)
   .then(() => {
+    // Inicia o servidor na porta definida
     app.listen(PORT, () => {
       console.log(`🚀 Servidor rodando na porta ${PORT}`);
     });
