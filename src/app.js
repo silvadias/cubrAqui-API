990292e7bccdef1f14aaf40756595f9f2957f868
silvadias <luisdias04@hotmail.com>
2024-12-22 13:38:45 -0300

    bug: Restruturado o app. js para funcionar com server.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ const userRoutes = require('./routes/userRoutes');
 const shiftRoutes = require('./routes/shiftRoutes');
 const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middlewares/errorHandler');
+const homeRoutes = require('./routes/homeRoutes'); // Nome correto
 
 const app = express();
 
+// Middleware para tratar JSON
 app.use(express.json());
+
+// Rotas
+app.use('/', homeRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/shifts', shiftRoutes);
+
+// Middleware de tratamento de erros
 app.use(errorHandler);
 
 module.exports = app;
