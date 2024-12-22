6dee07e937adf57d922d1777d14589704d5d70a2
silvadias <luisdias04@hotmail.com>
2024-12-22 13:48:03 -0300

    fix: Modificado estrutura para melhor ciálogo com arquivo .env e conecção com o banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,      // Nome do banco
+  process.env.DB_USER,      // Usuário
+  process.env.DB_PASSWORD,  // Senha
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host: process.env.DB_HOST,  // Nome do serviço Docker
+    port: process.env.DB_PORT,  // Porta interna MySQL
+    dialect: process.env.DB_DIALECT || 'mysql', // Tipo do banco de dados
   }
 );
 
-module.exports = db;
+module.exports = sequelize;
