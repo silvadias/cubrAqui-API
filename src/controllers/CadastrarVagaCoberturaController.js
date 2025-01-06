8ae30f99336cf5a7e00c213104c87a097d8215d3
silvadias <luisdias04@hotmail.com>
2025-01-06 05:12:15 -0300

    feat: cadastrar vaga cobertura controller.

---
+++ src/controllers/CadastrarVagaCoberturaController.js
@@ -0,0 +1,85 @@
+const VagaCobertura = require('../models/VagaCobertura');
+
+async function registrar(req, res){
+
+    try {
+        const id = req.tokenDecodificado.id;
+        const {            
+            valor,
+            servico,
+            dataInicio,
+            horaInicio,
+            dataFim,
+            horaFim,            
+            cep,
+            logradouro,
+            complemento,
+            bairro,
+            localidade,
+            uf,
+            estado,
+            regiao,
+            ibge,
+            gia,
+            ddd,
+            siafi,
+            numero,
+            bloco,
+            apartamento,
+            setor,
+            tipo_local,
+            referencia,
+            latitude,
+            longitude,
+            observacao
+        } = req.body
+
+        const VagaCoberturaAberta = await VagaCobertura.create({
+            idEmpresa: id,
+            valor,
+            servico,
+            dataInicio,
+            horaInicio,
+            dataFim,
+            horaFim,
+            cep,
+            logradouro,
+            complemento,
+            bairro,
+            localidade,
+            uf,
+            estado,
+            regiao,
+            ibge,
+            gia,
+            ddd,
+            siafi,
+            numero,
+            bloco,
+            apartamento,
+            setor,
+            tipo_local,
+            referencia,
+            latitude,
+            longitude,
+            observacao,
+
+          });
+      
+          return res.status(201).json({
+            message: 'Vaga de Cobertura criada com sucesso',
+            vaga: VagaCoberturaAberta});
+
+        
+    } catch (error) {
+
+         // ⚠️ Captura e registra erros no processo
+      console.error('❌ Erro ao registrar usuário:', error);
+      return res.status(500).json({
+        message: 'Erro ao registrar',
+        falha: error.message,});        
+    }
+
+}
+
+module.exports = { registrar };
