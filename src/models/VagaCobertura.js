545fa7a33c0a7625722b6dfef6bc2acb06576af9
silvadias <luisdias04@hotmail.com>
2025-01-06 03:18:57 -0300

    feat: Criado a model de Vaga de Cobertura.

---
+++ src/models/VagaCobertura.js
@@ -0,0 +1,68 @@
+const sequelize = require('../config/db');
+const { DataTypes } = require('sequelize');
+const EnderecoMixin = require('../mixin/EnderecoMixin');
+
+const VagaCobertura = sequelize.define('Vaga_Cobertura', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    idEmpresa: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'empresas', // Nome da tabela no banco de dados
+            key: 'id',
+        }
+    },
+    valor: {
+        type: DataTypes.DECIMAL(5, 2), // Máximo 5 dígitos, 2 casas decimais
+        allowNull: false,
+        validate: {
+            min: 100,
+            max: 200,
+        }
+    },
+    servico: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    dataInicio: {
+        type: DataTypes.DATEONLY, // Apenas data (YYYY-MM-DD)
+        allowNull: false,
+    },
+    horaInicio: {
+        type: DataTypes.STRING(5), // Formato HH:MM
+        allowNull: false,
+        validate: {
+            is: /^([0-1]\d|2[0-3]):([0-5]\d)$/, // Valida formato de hora
+        }
+    },
+    dataFim: {
+        type: DataTypes.DATEONLY, // Apenas data (YYYY-MM-DD)
+        allowNull: false,
+    },
+    horaFim: {
+        type: DataTypes.STRING(5), // Formato HH:MM
+        allowNull: false,
+        validate: {
+            is: /^([0-1]\d|2[0-3]):([0-5]\d)$/, // Valida formato de hora
+        }
+    },
+    ...EnderecoMixin,
+    observacao: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM('aberta', 'concorrência', 'atribuída', 'exercida', 'executada', 'fechada'),
+        allowNull: false,
+        defaultValue: 'aberta',
+    }
+}, {
+    tableName: 'vagas_coberturas', // Nome da tabela no banco de dados
+    timestamps: true, // Inclui createdAt e updatedAt
+});
+
+module.exports = VagaCobertura;
