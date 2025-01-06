402469bac36a10788faf8b282fda7e9183e0d0d8
silvadias <luisdias04@hotmail.com>
2025-01-06 03:04:38 -0300

    fix: Ajuste no nome da model no sequelize.

--- src/models/EnderecoEmpresa.js
+++ src/models/EnderecoEmpresa.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/db');
 const EnderecoMixin = require('../mixin/EnderecoMixin');
 const Empresa = require('./Empresa');  // Importa o modelo Empresa
 
-const EnderecoEmpresa = sequelize.define('Enderecos_Empresa', {
+const EnderecoEmpresa = sequelize.define('Endereco_Empresa', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
