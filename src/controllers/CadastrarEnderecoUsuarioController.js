cc7a27298d509d78c5d526e166751ecd96e34b82
silvadias <luisdias04@hotmail.com>
2025-01-07 23:26:01 -0300

    feat: Adicionado controlador para registrar endereço de usuário.

---
+++ src/controllers/CadastrarEnderecoUsuarioController.js
@@ -0,0 +1,82 @@
+const EnderecoUsuario = require('../models/EnderecoUsuario');
+
+async function registrar(req, res){
+
+    try {
+        const id = req.tokenDecodificado.id;
+        
+        const {               
+            cep,
+            logradouro,
+            complemento,
+            bairro,
+            localidade,
+            uf,
+            estado,
+            regiao,
+            ibge,
+            gia,
+            ddd,
+            siafi,
+            numero,
+            bloco,
+            apartamento,
+            setor,
+            tipo_local,
+            referencia,
+            latitude,
+            longitude,            
+        } = req.body
+
+        if (!cep || !numero) {
+            return res.status(400).json({ message: "CEP e número são obrigatórios." });
+          }
+          const idCadastrado = await Endereco.findAll({
+            where: {idUsuario : id}
+          });
+      
+          if (idCadastrado.length > 0) {
+            return res.status(409).json({ message: "Só é possível registar um endereço por usuário!"});
+          }
+
+        const EnderecoUsuarioRegistrado = await EnderecoUsuario.create({
+            idUsuario: id,            
+            cep,
+            logradouro,
+            complemento,
+            bairro,
+            localidade,
+            uf,
+            estado,
+            regiao,
+            ibge,
+            gia,
+            ddd,
+            siafi,
+            numero,
+            bloco,
+            apartamento,
+            setor,
+            tipo_local,
+            referencia,
+            latitude,
+            longitude,
+          });
+      
+          return res.status(201).json({
+            message: 'Registro de endereço do usuário criado com sucesso!',
+            EnderecoEmpresa: EnderecoUsuarioRegistrado});
+
+        
+    } catch (error) {
+
+         // ⚠️ Captura e registra erros no processo
+      console.error('❌ Erro ao registrar endereço do usuário:', error);
+      return res.status(500).json({
+        message: 'Erro ao registrar',
+        falha: error.message,});        
+    }
+
+}
+
+module.exports = { registrar };
